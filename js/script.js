e5b03151386508ad4e6afa1200092c81ae091a55
Castro <gustavocastrocs@gmail.com>
2019-09-02 08:58:46 -0300

    Animações JS

--- js/script.js
+++ js/script.js
@@ -1,12 +1,18 @@
 if (window.SimpleSlide) {
-  new SimpleSlide({
-    slide: "quote",
-    time: 5000
-  });
 
-  new SimpleSlide({
-    slide: "portfolio",
-    time: 5000,
-    nav: true
-  });
+	new SimpleSlide({
+		slide: "quote", // nome do atributo data-slide="principal"
+		time: 5000 // tempo de transição dos slides
+	});
+
+	new SimpleSlide({
+		slide: "portfolio",
+		time: 5000,
+		nav: true
+	});
+
+}
+
+if (window.SimpleAnime) {
+	new SimpleAnime();
 }
